c1aaed963690ce1ae371e4bc1d40f7aea2fbb682
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.local>
2020-04-18 16:32:45 -0700

    Server data almost complete

--- server/server.js
+++ server/server.js
@@ -2,15 +2,39 @@ const config = require('./config.json');
 const fetch = require('node-fetch');
 const { parse } = require('node-html-parser');
 
-function getData(querySelector) {
+// April 18 2020: This only gets the array of states
+// format: 
+// {
+//      state: string,
+//      total_cases: int,
+//      total_death: int
+// }
+function _getData(text) {
+    const root = parse(text);
+    const children = root.querySelector('#usa_table_countries_today tbody').childNodes;
+    const rows = children.filter(node => node.tagName === 'tr');
+    const result = [];
+    rows.forEach((row, i) => {
+        if (i !== 0) {
+            const tds = row.childNodes.filter(node => node.tagName === 'td');
+            result.push({
+                state: tds[0].text.trim(),
+                total_cases: tds[1].text.trim(),
+                total_death: tds[3].text.trim()
+            })
+        }
+    }); 
+    return result;
+}
+
+function getData() {
     return new Promise((acc, rej) => {
         fetch(config.data_url)
             .then(resp => {
                 return resp.text();
             })
             .then(text => {
-                const root = parse(text);
-                acc(root.querySelector('#usa_table_countries_today'));
+                acc(_getData(text));
             })
             .catch(err => {
                 rej(err);
--- server/server.test.js
+++ server/server.test.js
@@ -1,15 +1,11 @@
 const server = require('./server');
 
-test('get data should get table with id usa_table_countries_today', async () => {
-    const result = await server.getData();
-    expect(result.getAttribute('id')).toBe('usa_table_countries_today');
-});
 
-
-test('table should contain 52 items', async () => {
+test('test first item get', async () => {
     const result = await server.getData();
-    const rows = result.querySelector('tbody');
-    const states = rows.childNodes.filter(node => node.tagName === 'tr');
-    expect(states.length).toBe(52);
+    expect(result[0].state).toEqual('New York');
+    expect(result[0].total_cases).not.toEqual(undefined);
+    expect(result[0].total_death).not.toEqual(undefined);
+    expect(result.length).toEqual(51);
 });
  
